899fc522e34b24160d1873812d247797488b208e
Eduardoczi <eduardoczigler@gmail.com>
2025-06-05 11:20:25 -0300

    Delete AtendimentoAoCliente.java

--- AtendimentoAoCliente.java
+++
@@ -1,7 +0,0 @@
-package Setor;
-
-public class AtendimentoAoCliente extends Setores {
-    public AtendimentoAoCliente(String nome, int qtdFuncionarios) {
-        super(nome, qtdFuncionarios);
-    }
-}
